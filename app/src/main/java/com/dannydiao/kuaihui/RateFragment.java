d3edeaa95da5630790ed39393be9e9d9b573f4f3
DiaoSu <723714386@qq.com>
2019-05-08 17:08:13 +0800

    汇率界面基本完成

--- app/src/main/java/com/dannydiao/kuaihui/RateFragment.java
+++ app/src/main/java/com/dannydiao/kuaihui/RateFragment.java
@@ -3,20 +3,32 @@ package com.dannydiao.kuaihui;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.service.quicksettings.Tile;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RateFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    List<String> Title = new ArrayList<>();
+    List<String> Current = new ArrayList<>();
+    List<String> Hint = new ArrayList<>();
+    RecyclerView recyclerView;
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -26,15 +38,6 @@ public class RateFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RateFragment newInstance(String param1, String param2) {
         RateFragment fragment = new RateFragment();
         Bundle args = new Bundle();
@@ -57,10 +60,56 @@ public class RateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rate, container, false);
+        View v = inflater.inflate(R.layout.fragment_rate, container, false);
+        initTitle();
+        initCurrency();
+        TextView refresh_time = v.findViewById(R.id.refresh_time_2);
+        recyclerView = v.findViewById(R.id.recycler_view_2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RateListAdapter rateListAdapter = new RateListAdapter(Title,Current,Hint);
+        recyclerView.setAdapter(rateListAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL_LIST));
+
+
+
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    public void initTitle() {
+
+        Title.add("美元 USD");
+        Title.add("港币 HKD");
+        Title.add("欧元 EUR");
+        Title.add("日元 JPY");
+        Title.add("英镑 GBP");
+        Title.add("韩元 KRW");
+        Title.add("加拿大元 CAD");
+        Title.add("澳大利亚元 AUD");
+        Title.add("新台币 TWD");
+        Title.add("新加坡元 SGD");
+        Title.add("泰铢 THB");
+        Title.add("澳门元 MOP");
+
+    }
+
+    public void initCurrency() {
+
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+        Current.add("0");
+
+    }
+
 
     }
 
---
+++ app/src/main/java/com/dannydiao/kuaihui/RateListAdapter.java
@@ -0,0 +1,86 @@
+package com.dannydiao.kuaihui;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RateListAdapter extends RecyclerView.Adapter<RateListAdapter.VH> {
+
+    public static class VH extends RecyclerView.ViewHolder{
+        public final TextView Title;
+        public final TextView Current;
+        public final ImageView Pic;
+        public final TextView Hint;
+        public VH(View v){
+            super(v);
+            Title = v.findViewById(R.id.List_Title_2);
+            Current = v.findViewById(R.id.List_Current_2);
+            Pic = v.findViewById(R.id.List_Pic_2);
+            Hint = v.findViewById(R.id.hint);
+
+        }
+    }
+
+    private List<String> TitleArray;
+    private List<String> CurrentArray;
+    private List<String> Hint;
+
+
+    public RateListAdapter(List<String> data1,List<String> data2,List data3){
+        this.TitleArray = data1;
+        this.CurrentArray = data2;
+        this.Hint = data3;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH vh, int i) {
+        vh.Title.setText(TitleArray.get(i));
+        vh.Current.setText(CurrentArray.get(i));
+        vh.Hint.setText("100" + TitleArray.get(i) + "/人民币");
+        switch (i){
+            case 0:vh.Pic.setImageResource(R.drawable.usd);
+                break;
+            case 1:vh.Pic.setImageResource(R.drawable.hkd);
+                break;
+            case 2:vh.Pic.setImageResource(R.drawable.eur);
+                break;
+            case 3:vh.Pic.setImageResource(R.drawable.jpy);
+                break;
+            case 4:vh.Pic.setImageResource(R.drawable.gbp);
+                break;
+            case 5:vh.Pic.setImageResource(R.drawable.krw);
+                break;
+            case 6:vh.Pic.setImageResource(R.drawable.cad);
+                break;
+            case 7:vh.Pic.setImageResource(R.drawable.aud);
+                break;
+            case 8:vh.Pic.setImageResource(R.drawable.twd);
+                break;
+            case 9:vh.Pic.setImageResource(R.drawable.sgd);
+                break;
+            case 10:vh.Pic.setImageResource(R.drawable.thb);
+                break;
+            case 11:vh.Pic.setImageResource(R.drawable.mop);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return TitleArray.size();
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rate_list,viewGroup,false);
+        return new VH(v);
+    }
+}
+
