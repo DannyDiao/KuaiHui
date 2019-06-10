a857b0e7bc4658a33ab7b83273d13f5886996df6
DanyDiao <723714386@qq.com>
2019-06-10 11:44:30 +0800

    正在写Exchange后端逻辑

--- back-end/main.js
+++ back-end/main.js
@@ -12,9 +12,13 @@ var app = express();
 var httpsServer = https.createServer(options,app);
 
 var CurrencyData = fs_1.readFileSync(CurrencyPath,'utf8');
+
 app.get('/KuaiHuiCurrency',function(req,res){
     console.log('received request');
     res.send(CurrencyData);
 });
 
+app.get('/KuaiHuiExchange',function(req,res){
+  
+})
 httpsServer.listen(3500);
