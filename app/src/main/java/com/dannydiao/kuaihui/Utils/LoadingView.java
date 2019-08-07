915c1e61ba46d11100c2b46658cc6723fe4e0a23
DannyDiao <diaosu@diaosudev.cn>
2019-08-07 13:15:36 +0800

    Init commit.

---
+++ app/src/main/java/com/dannydiao/kuaihui/Utils/LoadingView.java
@@ -0,0 +1,15 @@
+package com.dannydiao.kuaihui.Utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dannydiao.kuaihui.R;
+
+public class LoadingView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_view);
+    }
+}
